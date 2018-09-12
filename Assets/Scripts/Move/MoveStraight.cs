afbe1e90353dfb908b92b669caf4806eed51e293
4057492 <837233905@qq.com>
2018-09-12 22:51:01 +0800

    9

---
+++ Assets/Scripts/Move/MoveStraight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStraight : MoveType {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (0, speed, 0));
+	}
+
+	public override void Setparament (float parament)
+	{
+		speed = parament;
+	}
+
+
+}
---
+++ Assets/Scripts/Move/MoveType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveType : MonoBehaviour {
+
+	public virtual void Setparament(float parament){
+		
+	}
+}
